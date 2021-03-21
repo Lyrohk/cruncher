d2eb2b7f9d33a2dc6d504d3e29e792ab9846fa5d
layla <layla.rohkohl@jcnetwork.de>
2021-03-21 15:13:55 +0100

    reformat

--- R/construct order function.R
+++ R/construct order function.R
@@ -1,13 +1,13 @@
 # Hidden function to make order
 make_order <- function(order_by, sort_direction) {
   # Construct list
-  inner_list <- vector(mode="list", length=2)
+  inner_list <- vector(mode = "list", length = 2)
   names(inner_list) <- c("field_id", "sort")
   inner_list[[1]] <- order_by
   inner_list[[2]] <- sort_direction
   
   # Return list
-  order <- vector(mode="list", length=1)
+  order <- vector(mode = "list", length = 1)
   order[[1]] <- inner_list
   order
 }
--- R/construct predicate function.R
+++ R/construct predicate function.R
@@ -1,7 +1,7 @@
 # Function to make predicate in query
 make_predicate <- function(field, operator, value) {
   # Construct list
-  predicate <- vector(mode="list", length=4)
+  predicate <- vector(mode = "list", length = 4)
   names(predicate) <- c("type", "field_id", "operator_id", "values")
   predicate[[1]] <- "predicate"
   predicate[[2]] <- field
