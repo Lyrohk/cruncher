0cf0c48369561d0b131ab51936fd8e6e626e74c8
layla <layla.rohkohl@jcnetwork.de>
2021-04-16 11:05:14 +0200

    add cat info header to output

--- R/printError.R
+++ R/printError.R
@@ -11,6 +11,7 @@
 #'
 printError <- function(response_code) {
   if (response_code == 400) {
+    cat("Information:")
     cat(paste("Request returned the error code", response_code))
     cat("One of the following issues are likely the cause:")
     cat(c("Multiple pagination parameters are specified in the api call", 
@@ -22,18 +23,25 @@ printError <- function(response_code) {
             "The format isn't supported."))
     cat("Please check and try again.")
   } else if (response_code == 401) {
+    cat("Information:")
     cat(paste("The specified API_KEY is invalid and request and returned error code", response_code))
   } else if (response_code == 404) {
+    cat("Information:")
     cat(paste("The specified entity collection id is invalid, the resource cannot be found by its name , or the field_id does not exist. The request returned the error code", response_code))
   } else if (response_code == 403) {
+    cat("Information:")
     cat(paste("Request is asking for more than 1000 results and returned error code", response_code))
   } else if (response_code == 409) {
+    cat("Information:")
     cat(paste("Too many requests are made and you're rate-limited. The status code returned the error ", response_code))
   } else if (response_code == 429) {
+    cat("Information:")
     cat(paste("Too many concurrent requests are made and the error code returned", response_code))
   } else if (response_code == 500) {
+    cat("Information:")
     cat(paste("A generic error occurred, likely due to an internal problem. The error code is", response_code))
   } else if (response_code == 502) {
+    cat("Information:")
     cat(paste("The service is currenctly unavailable due to an outage. The error code is", response_code))
   } else {
     # Unknown status code, return as is
