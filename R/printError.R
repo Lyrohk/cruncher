7326dfe79a2b8d652147923a7decf81e6655f16a
layla <layla.rohkohl@jcnetwork.de>
2021-04-16 11:00:42 +0200

    cat instead of print or warnings

--- R/printError.R
+++ R/printError.R
@@ -11,32 +11,32 @@
 #'
 printError <- function(response_code) {
   if (response_code == 400) {
-    warnings(paste("Request returned the error code", response_code))
-    print("One of the following issues are likely the cause:")
-    print(c("Multiple pagination parameters are specified in the api call", 
+    cat(paste("Request returned the error code", response_code))
+    cat("One of the following issues are likely the cause:")
+    cat(c("Multiple pagination parameters are specified in the api call", 
             "Api call contains non-json body or an invalid json search request",
             "The request uri is invalid",
             "Query timeout is exceeded",
             "The specified operator_id is unknown or invalid",
             "The specified values are invalid",
             "The format isn't supported."))
-    print("Please check and try again.")
+    cat("Please check and try again.")
   } else if (response_code == 401) {
-    warnings(paste("The specified API_KEY is invalid and request and returned error code", response_code))
+    cat(paste("The specified API_KEY is invalid and request and returned error code", response_code))
   } else if (response_code == 404) {
-    warnings(paste("The specified entity collection id is invalid, the resource cannot be found by its name , or the field_id does not exist. The request returned the error code", response_code))
+    cat(paste("The specified entity collection id is invalid, the resource cannot be found by its name , or the field_id does not exist. The request returned the error code", response_code))
   } else if (response_code == 403) {
-    warnings(paste("Request is asking for more than 1000 results and returned error code", response_code))
+    cat(paste("Request is asking for more than 1000 results and returned error code", response_code))
   } else if (response_code == 409) {
-    warnings(paste("Too many requests are made and you're rate-limited. The status code returned the error ", response_code))
+    cat(paste("Too many requests are made and you're rate-limited. The status code returned the error ", response_code))
   } else if (response_code == 429) {
-    warnings(paste("Too many concurrent requests are made and the error code returned", response_code))
+    cat(paste("Too many concurrent requests are made and the error code returned", response_code))
   } else if (response_code == 500) {
-    warnings(paste("A generic error occurred, likely due to an internal problem. The error code is", response_code))
+    cat(paste("A generic error occurred, likely due to an internal problem. The error code is", response_code))
   } else if (response_code == 502) {
-    warnings(paste("The service is currenctly unavailable due to an outage. The error code is", response_code))
+    cat(paste("The service is currenctly unavailable due to an outage. The error code is", response_code))
   } else {
     # Unknown status code, return as is
-    warning(paste("Error", response_code))
+    cat(paste("Error", response_code))
   }
 }
