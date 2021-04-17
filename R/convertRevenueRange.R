967c18127ed8772f97ac929d0370dccd36b0dafb
layla <layla.rohkohl@jcnetwork.de>
2021-04-17 23:59:31 +0200

    adjusted for missing or na input

--- R/convertRevenueRange.R
+++ R/convertRevenueRange.R
@@ -10,24 +10,30 @@
 #'
 # Revenue Converter Function
 convertRevenueRange <- function(rev_range) {
-  if (rev_range == "r_00000000") {
-    return("Less than $1M")
-  } else if (rev_range == "r_00001000") {
-    return("$1M to $10M")
-  } else if (rev_range == "r_00010000") {
-    return("$10M to $50M")
-  } else if (rev_range == "r_00050000") {
-    return("$50M to $100M")
-  } else if (rev_range == "r_00100000") {
-    return("$100M to $500M")
-  } else if (rev_range == "r_00500000") {
-    return("$500M to $1B")
-  } else if (rev_range == "r_01000000") {
-    return("$1B to $10B")
-  } else if (rev_range == "r_10000000") {
-    return("$10B+")
+  # Check if missing
+  if (rev_range == "" | is.na(rev_range)) {
+    return(NA)
   } else {
-    # Keep it as is
-    return(rev_range)
+    # Convert the revenue ranges if they are recognized
+    if (rev_range == "r_00000000") {
+      return("Less than $1M")
+    } else if (rev_range == "r_00001000") {
+      return("$1M to $10M")
+    } else if (rev_range == "r_00010000") {
+      return("$10M to $50M")
+    } else if (rev_range == "r_00050000") {
+      return("$50M to $100M")
+    } else if (rev_range == "r_00100000") {
+      return("$100M to $500M")
+    } else if (rev_range == "r_00500000") {
+      return("$500M to $1B")
+    } else if (rev_range == "r_01000000") {
+      return("$1B to $10B")
+    } else if (rev_range == "r_10000000") {
+      return("$10B+")
+    } else {
+      # Keep it as is
+      return(rev_range)
+    }
   }
 }
