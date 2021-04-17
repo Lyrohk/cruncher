d3d26185dc4a894b49e8e120c9a3127b6702f117
layla <layla.rohkohl@jcnetwork.de>
2021-04-17 15:53:52 +0200

    category group parser, lookup single and multiple functions added

---
+++ R/lookupCategoryGroup.R
@@ -0,0 +1,52 @@
+#' Function to lookup a single category group over the Entity Lookup API Endpoint
+#'
+#'@param id UUID or permalink of the category group you wish to look up
+#'@param parse TRUE or FALSE. By default TRUE. If set to FALSE, it will return the data directly from the JSON, if set to TRUE, it will parse it into a data.frame object
+#'@return either a data.frame (if parse = TRUE) or a list (if parse = FALSE)
+#'
+#' @author Layla Rohkohl, \email{byehity@gmail.com}
+#'
+#' @examples
+#' lookupCategoryGroup("id")
+#'
+#' @import httr
+#' @import jsonlite
+#' @export
+#'
+# Call the function to get you information about a category group entity
+lookupCategoryGroup <- function(id, please_parse = TRUE)  {
+  
+  # Check that API_KEY exists
+  if (!exists("API_KEY")) {
+    stop("Please set a valid user key to API_KEY as an environmental variable or for use setAPIKey() to do it for you.")
+  }
+  
+  # Check that id has been specified 
+  if (missing(id)) {
+    stop("Please enter a uuid or permalink for the person you wish to look up. You can find them manually from the browser or csv files.")
+  }
+  
+  # Create the path
+  url <- paste0("https://api.crunchbase.com/api/v4/entities/category_groups/", id, "?card_ids=fields&user_key=", API_KEY)
+  
+  # Make http GET request
+  response <- RETRY(verb = "GET", url = url) # Could use GET but someone may apply it with a list
+  
+  # Check if we get valid data, if not return error core
+  if (response$status_code == 200) {
+    data <- fromJSON(rawToChar(response$content))
+    # Check if parsing is wanted
+    if (please_parse) {
+      
+      # Parse category group to return a dataframe
+      return(parseCategoryGroup(data$cards$fields))
+      
+    } else {
+      # Return data
+      return(data)
+    }
+  } else {
+    # Print error code
+    printError(response$status_code)
+  }
+}
