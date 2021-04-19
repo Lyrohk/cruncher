0ef82da34407ed8fb7852b676a5d5cbebf28dcb4
layla <layla.rohkohl@jcnetwork.de>
2021-04-19 19:49:58 +0200

    event appearances single, multiple lookup and parsing function

--- R/lookupEventAppearances.R
+++ R/lookupEventAppearances.R
@@ -1,19 +1,19 @@
-#' Function to lookup a single event appearance over the Entity Lookup API Endpoint
+#' Function to lookup multiple event appearances over the Entity Lookup API Endpoint
 #'
-#'@param id UUID or permalink of the event appearance you wish to look up
+#'@param id UUID or permalink of the event appearances you wish to look up
 #'@param parse TRUE or FALSE. By default TRUE. If set to FALSE, it will return the data directly from the JSON, if set to TRUE, it will parse it into a data.frame object
 #'@return either a data.frame (if parse = TRUE) or a list (if parse = FALSE)
 #'
 #' @author Layla Rohkohl, \email{byehity@gmail.com}
 #'
 #' @examples
-#' lookUpEventAppearance("uuid")
+#' lookUpEventAppearances(c("uuid1", "uuid2"))
 #'
 #' @import httr
 #' @import jsonlite
 #' @export
 #'
 # Call the function to get you information about an event appearance
-lookupEventAppearance <- function(id, please_parse = TRUE)  {
-  return(lookupEntity(id = id, path = "event_appearances", please_parse = please_parse))
+lookupEventAppearances <- function(event_appearances, please_parse = TRUE)  {
+  return(lookupEntities(entities = event_appearances, path = "event_appearances", please_parse = please_parse))
 }
---
+++ R/parseEventAppearance.R
@@ -0,0 +1,64 @@
+#' Function to parse Event Appearance Entity from JSON list to Data.frame
+#'
+#'@param data Returned fields card response in json list form for the entity
+#'@return  a data.frame of parsed output
+#'
+#' @author Layla Rohkohl, \email{byehity@gmail.com}
+#'
+#' @examples
+#' parseInvestment(data$cards$fields)
+#'
+#'@import stringr
+#'
+parseEventAppearance <- function(data) {
+  # Identifier parsing
+  ids <- parseIdentifier(data[["identifier"]], "identifier")
+  event_identifier <- parseIdentifier(data[["event_identifier"]], "event_identifier")
+  participant_identifier <- parseIdentifier(data[["participant_identifier"]], "participant_identifier")
+
+  # Simple parsing
+  appearance_type <- str_replace_all(simpleParse(data[["appearance_type"]]),
+                                     pattern = c(
+                                       "contestant" = "Contestant",
+                                       "exhibitor" = "Exhibitor",
+                                       "organizer" = "Organizer",
+                                       "speaker" = "Speaker",
+                                       "sponsor" = "Sponsor"
+                                     ))
+  rank <- simpleParse(data[["rank"]])
+  short_description <- simpleParse(data[["short_description"]])
+  starts_on <- simpleParse(data[["event_starts_on"]])
+  created_at <- simpleParse(data[["created_at"]])
+  updated_at <- simpleParse(data[["updated_at"]])
+
+  # List parsing
+  location <- listParse(data[["event_location_identifiers"]])
+
+
+
+  # Put into one dateframe
+  df <- data.frame(cbind(  ids,
+                           event_identifier,
+                           participant_identifier,
+                           short_description,
+                           appearance_type,
+                           rank,
+                           starts_on,
+                           created_at ,
+                           updated_at, # Simple parsing done
+                           location
+
+  ))
+
+  # Adjust classes
+  # as numeric
+  df$rank <- as.numeric(df$rank)
+
+  # as date
+  df$starts_on <- as.Date(df$starts_on)
+  df$created_at <- as.Date(df$created_at)
+  df$updated_at <- as.Date(df$updated_at)
+
+  # Return dataframe
+  return(df)
+}
