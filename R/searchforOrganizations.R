fdecf26f39c6614f60512057dc3f9d88196dc3bd
layla <layla.rohkohl@jcnetwork.de>
2021-06-11 12:47:34 +0200

    documentation augmented to accuracy of user set limit and parsing

--- R/searchforOrganizations.R
+++ R/searchforOrganizations.R
@@ -3,7 +3,7 @@
 #'@param search_conditions the search condition data.frame that the funding_rounds have to fulfill
 #'@param order_by (Optional) field by which to sort the results
 #'@param sort_by (Optional) sort direction to sort the order field by. Either 'asc' or 'desc'.
-#'@param result_limit (Optional) numeric limit of the number of results you'd like to have. By default, all results will be returned to you.
+#'@param result_limit (Optional) numeric limit of the number of results you'd like to have. By default, all results will be returned to you. Only accurate to 1000 level, no smaller iterations.
 #'@param uuids_only (Optional) Logical if you want to have only the uuids of the searched for entities returned. By default, after the uuids are retrieved, the information for all entities will be looked up over the entity lookup API endpoint, which takes longer.
 #'@return a data.frame
 #'
